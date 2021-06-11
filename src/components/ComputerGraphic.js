7fdd19fa74b3c69011e07ce32b118eff406c791c
Emily Blair <emilyblair96@gmail.com>
2021-06-10 19:40:20 -0500

    Responsiveness updates

--- src/components/ComputerGraphic.js
+++ src/components/ComputerGraphic.js
@@ -1,16 +1,33 @@
+import { useTheme } from "@material-ui/core/styles";
 import { Grid, Container } from '@material-ui/core';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 import computerGraphic from '../images/computer-screen-graphic.png'
 
 export default function ComputerGraphic() {
+
+  let picHeight;
+
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const responsiveGraphic = 
+  matches 
+  ? <img 
+  src={computerGraphic} 
+  style ={{height: '40em'}}
+  />
+  : <img 
+  src={computerGraphic} 
+  style ={{height: '18em'}}
+  />
+
   return (
     <Grid
       container
       justify='center'
     >
-    <img 
-    src={computerGraphic} 
-    style ={{height: '40em'}}
-    />
+    {responsiveGraphic}
     </Grid>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,33 +6,27 @@ import GridBase from "@material-ui/core/Grid";
 
 export default function Header() {
 
-//   const Grid = styled(GridBase)`
-//   .MuiGrid-root {
-//     flex-grow: 1;
-//   }
-// `
-
   return (
-    <div>
+//     <div>
     
 
-<Grid
-  container
-  spacing={0}
-  direction="row"
-  // alignItems="center"
-  justify="center"
-  // style={{ minWidth: '200vw' }}
->
+// <Grid
+//   container
+//   spacing={0}
+//   direction="row"
+//   // alignItems="center"
+//   justify="center"
+//   // style={{ minWidth: '200vw' }}
+// >
 
-  <Grid 
-    item xs={12}
-    style={{width: '200vw'}}
+//   <Grid 
+//     item xs={12}
+//     style={{width: '200vw'}}
   
-  >
-      <h1 style={{textAlign: 'center' }}>FULL STACK</h1>
-      </Grid>
-      </Grid>
-    </div>
+//   >
+//       </Grid>
+//       </Grid>
+//     </div>
+  <h1 style={{textAlign: 'center' }}>FULL STACK</h1>
   )
 }
