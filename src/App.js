d1d193201dce7ab295af193ee18b6bf9d49aabff
Emily Blair <emilyblair96@gmail.com>
2021-07-20 11:11:54 -0600

    Add obvious text to check deployment

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         <Header />
         <Subheader text="SOFTWARE ENGINEER"/>
         <ComputerGraphic />
+        <h1>this is a test.</h1>
         <Divider style={{ margin: "2em" }} />
         <Subheader text="ABOUT ME."/>
         <AboutMe />
